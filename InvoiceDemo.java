d0631d606d5d38d8906d8c5b9bb123f7dc59626f
prasanth8745 <54884765+prasanth8745@users.noreply.github.com>
2021-02-06 17:43:17 +0530

    Update InvoiceDemo.java

--- InvoiceDemo.java
+++ InvoiceDemo.java
@@ -1,3 +1,11 @@
+1. Create a class called Invoice that a hardware store might use to represent an invoice for an item sold at the store. An Invoice should include four pieces of
+information as instance variables-a part number(type String),a partdescription(type String),a quantity of the item being purchased (type int) and a
+price per item (double). Your class should have a constructor that initializes thefour instance variables. Provide a set and a get method for each instance variable.
+In addition, provide a method named getInvoiceAmount that calculates the invoiceamount (i.e., multiplies the quantity by the price per item), then returns the
+amount as a double value. If the quantity is not positive, it should be set to 0. Ifthe price per item is not positive, it should be set to 0.0. Write a test application
+named InvoiceTest that demonstrates class Invoice’s capabilities.
+
+    
 import  java.util.*;
 class Invoice
 {
